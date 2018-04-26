44616fb77a138fe70b288ea74d12a671f771f034
Tahir Javed <tahir.javed@digitalsputnik.com>
2018-04-26 14:24:42 +0300

    Add lamps window fix

---
+++ Assets/Scripts/ButtonCancel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCancel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        var cancelButton = gameObject.GetComponent<Button>();
+        cancelButton.onClick.AddListener(TaskHideParent);
+
+    }
+
+    private void TaskHideParent() {
+        transform.parent.gameObject.SetActive(false);
+    }
+
+
+
+}
