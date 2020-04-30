c5a044c57a7dd71d892aee4e33b04a8fbae5b731
taavetmaask <taavetmaask.tm@gmail.com>
2020-04-30 13:41:35 +0300

    iOS build settings

--- Assets/Vendor/Bluetooth/Editor/IOSBluetoothPostBuild.cs
+++ Assets/Vendor/Bluetooth/Editor/IOSBluetoothPostBuild.cs
@@ -14,23 +14,6 @@ public class IOSBluetoothPostBuild
     [PostProcessBuild]
 	public static void OnPostprocessBuild(BuildTarget _, string buildPath)
 	{
-        //string pbxProjectPath = PBXProject.GetPBXProjectPath(buildPath);
-        //string plistPath = Path.Combine(buildPath, "Info.plist");
-        //PBXProject pbxProject = new PBXProject();
-        //pbxProject.ReadFromFile(pbxProjectPath);
-        //string targetGUID = pbxProject.GetUnityFrameworkTargetGuid();
-        //pbxProject.AddBuildProperty(targetGUID, "OTHER_LDFLAGS", "-framework Photos");
-        //pbxProject.AddBuildProperty(targetGUID, "OTHER_LDFLAGS", "-framework MobileCoreServices");
-        //pbxProject.AddBuildProperty(targetGUID, "OTHER_LDFLAGS", "-framework ImageIO");
-        //pbxProject.RemoveFrameworkFromProject(targetGUID, "Photos.framework");
-        //File.WriteAllText(pbxProjectPath, pbxProject.WriteToString());
-        //PlistDocument plist = new PlistDocument();
-        //plist.ReadFromString(File.ReadAllText(plistPath));
-        //PlistElementDict rootDict = plist.root;
-        //rootDict.SetString("NSPhotoLibraryUsageDescription", PHOTO_LIBRARY_USAGE_DESCRIPTION);
-        //rootDict.SetString("NSPhotoLibraryAddUsageDescription", PHOTO_LIBRARY_USAGE_DESCRIPTION);
-        //File.WriteAllText(plistPath, plist.WriteToString());
-
         string plistPath = Path.Combine(buildPath, "Info.plist");
         var propertyList = new PlistDocument();
 		propertyList.ReadFromFile(plistPath);
