a62c75b571ca5d7f45e397d49964c5521a1c2e3a
MJoosep <joosepmartin1999@gmail.com>
2020-05-20 13:36:44 +0300

    encrypted passwords test
    -password will be changed into hash and then used to generate set client mode packet

--- Assets/Scripts/UI/Menus/Inspector/ClientModeMenu.cs
+++ Assets/Scripts/UI/Menus/Inspector/ClientModeMenu.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 using VoyagerApp.Lamps;
@@ -63,6 +66,34 @@ namespace VoyagerApp.UI.Menus
                 if (WorkspaceUtils.SelectedVoyagerLamps.Count > 0 && string.IsNullOrEmpty(ssidField.text))
                     ssidField.text = WorkspaceUtils.SelectedVoyagerLamps[0].activePattern;
             }
+        }
+
+        public string WPA_PSK(string ssid, string password)
+        {
+            byte[] ssidBytes = Encoding.ASCII.GetBytes(ssid);
+            byte[] passwordBytes = Encoding.ASCII.GetBytes(password);
+            Rfc2898DeriveBytes pbkdf2;
+            //little magic here
+            //Rfc2898DeriveBytes class has restriction of salt size to >= 8
+            //but rfc2898 not (see http://www.ietf.org/rfc/rfc2898.txt)
+            //we use Reflection to setup private field to avoid this restriction
+
+            if (ssid.Length >= 8)
+            {
+                pbkdf2 = new Rfc2898DeriveBytes(passwordBytes, ssidBytes, 4096);
+            }
+            else
+            {
+                //use dummy salt here, we replace it later vie reflection
+                pbkdf2 = new Rfc2898DeriveBytes(password, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }, 4096);
+
+                var saltField = typeof(Rfc2898DeriveBytes).GetField("m_salt", BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.Instance);
+                saltField.SetValue(pbkdf2, ssidBytes);
+            }
+
+            //get 256 bit PMK key
+            byte[] resultBytes = pbkdf2.GetBytes(32);
+            return BitConverter.ToString(resultBytes).Replace("-", "");
         }
 
         #region Lamp SSIDS
@@ -181,7 +212,7 @@ namespace VoyagerApp.UI.Menus
                 var client = NetUtils.VoyagerClient;
 
                 foreach (var lamp in WorkspaceUtils.SelectedLamps)
-                    client.TurnToClient(lamp, ssid, password);
+                    client.TurnToClient(lamp, ssid, WPA_PSK(ssid, password).ToLower());
 
                 GetComponentInParent<InspectorMenuContainer>().ShowMenu(null);
             }
